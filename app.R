9c187d39ef2e1cd49e41a7d54de636050c331037
Kieran Hannigan <overyonderstudios@gmail.com>
2016-08-09 18:11:59 +0800

    UI overhaul. Added walkthrough boilerplate.

--- app.R
+++ app.R
@@ -1,5 +1,9 @@
+#Load required libraries
 library(shiny)
 library(shinydashboard)
+library(shinyjs)
+library(matrixStats)
+library(DT)
 
 ## ------------------------------
 ## UI functionality
@@ -7,70 +11,101 @@ library(shinydashboard)
 
 ui <- dashboardPage(
   dashboardHeader(title = "Reliability Application"),
+
   dashboardSidebar(
     sidebarMenu(
-      menuItem("Grand Process Analysis", tabName = "grand-process-analysis", icon = icon("dashboard"), badgeLabel = "beta", badgeColor = "purple"),
-      menuItem("Settings", tabName = "settings", icon = icon("gears"))
+      menuItem("Process Analysis", tabName = "process-analysis", icon = icon("dashboard"), badgeLabel = "beta", badgeColor = "purple"),
+      menuItem("Settings", tabName = "settings", icon = icon("gears")),
+      actionButton("helpButton", "Help", icon = icon("question-circle"), class="btn-info")
     )
   ),
+
   dashboardBody(
+    useShinyjs(),
     tags$head(
-      tags$link(rel = "stylesheet", type = "text/css", href = "custom.css")
+      tags$link(rel = "stylesheet", href = "css/custom.css"),
+      tags$link(rel = "stylesheet", href = "css/bootstrap-tour.min.css"),
+      tags$head(tags$script(src="js/bootstrap-tour.min.js", type="text/javascript")),
+      tags$head(tags$script('$(document).ready(function() {
+                                var tour = new Tour({
+                                  steps: [
+                                  {
+                                    title: "Walkthrough",
+                                    content: "This will be a walkthrough very soon!"
+                                  }
+                                  ],
+                                  orphan: true,
+                                  backdrop: true
+                                });
+                                tour.init();
+                                tour.start();
+                            });',
+                            type='text/javascript'))
     ),
+
     tabItems(
+
       tabItem(
-        tabName = "grand-process-analysis",
-        h2("Grand Process Analysis"),
+        tabName = "process-analysis",
+        h2("Process Analysis"),
         fluidRow(
           column(
             width = 8,
-            fluidRow(
-              width = NULL,
-                  infoBox("Grand Process Parameters", p(span(HTML("X&#x33F;"), "= 50"), br(),
-                                                        span(HTML("R&#x305;"), "= 10")
-                                                        ), icon = icon("info-circle")),
-                  infoBox("Fitted Distribution Type", "Normal", icon = icon("area-chart")),
-                  infoBox("Fitted Distribution Parameters", p(span(HTML("&mu;"), "= 50"), br(), span(HTML("&sigma;"), "= 5")), icon = icon("info"))
-            ),
             tabBox(
               width = NULL,
-              id = "grand-process-tabBox",
               tabPanel(
-                "Control Chart",
-                plotOutput("controlChart",
+                "SPC Chart",
+                plotOutput("SPCChart",
                            click = "plot_click",
                            dblclick = "plot_dblclick",
                            hover = "plot_hover",
                            brush = "plot_brush")
-                ),
+              ),
               tabPanel(
                 "Table View",
-                dataTableOutput('tableView')
+                DT::dataTableOutput('tableView')
               )
             )
           ),
           column(
             width = 4,
-            box(
+            tabBox(
               width = NULL,
-              title = "Upload Samples",
-              "Select a group or group-set in csv format.",
-              fileInput('file1', '',
-                        accept = c(
-                          'text/csv',
-                          'text/comma-separated-values',
-                          'text/tab-separated-values',
-                          'text/plain',
-                          '.csv',
-                          '.tsv'
-                        )
+              tabPanel(
+                "Add Samples",
+                p("Add samples groups one by one."),
+                textInput("addSampleTextInput", "Sample Value", "0"),
+                actionButton("addSampleActionButton", "Add Sample To Group", icon = icon("plus")),
+                br(),br(),
+                strong("Group:"),
+                br(),
+                DT::dataTableOutput('addGroupTable'),
+                br(),
+                actionButton("removeSamplesActionButton", "Remove Selected Samples From Group", icon = icon("minus")),
+                actionButton("clearGroupActionButton", "Clear Group", icon = icon("times")),
+                actionButton("addGroupActionButton", "Add Group To Process", icon = icon("plus"))
+              ),
+              tabPanel(
+                padding = 20,
+                "Upload Samples",
+                p("Select a sample group in csv format."),
+                fileInput('file1', '',
+                          accept = c(
+                            'text/csv',
+                            'text/comma-separated-values',
+                            'text/tab-separated-values',
+                            'text/plain',
+                            '.csv',
+                            '.tsv'
+                          )
+                )
               )
             ),
             box(
               width = NULL,
               title = "Results",
               p(span(HTML("&alpha;"))," Value: 0.05"),
-              textOutput('contents'),
+              textOutput("contents"),
               fluidRow(
                 valueBoxOutput("cpkBox", width = 6),
                 valueBoxOutput("totalAreaPercentageBox", width = 6)
@@ -82,35 +117,10 @@ ui <- dashboardPage(
           )
         )
       ),
+
       tabItem(
         tabName = "settings",
         h2("Settings"),
-        fluidRow(
-          box(
-            width = 6,
-            column(
-              width = 8,
-              textInput("usl-textInput", "Set USL", value = "70")
-            ),
-            column(
-              width = 4,
-              actionButton("usl-actionButton", "Set")
-            )
-          )
-        ),
-        fluidRow(
-          box(
-            width = 6,
-            column(
-              width = 8,
-              textInput("lsl-textInput", "Set LSL", value = "30")
-            ),
-            column(
-              width = 4,
-              actionButton("lsl-actionButton", "Set")
-            )
-          )
-        ),
         fluidRow(
           box(
             width = 12, height = 200, status = "danger", solidHeader = TRUE, title = "Danger Zone!",
@@ -120,6 +130,7 @@ ui <- dashboardPage(
           )
         )
       )
+
     )
   )
 )
@@ -130,35 +141,111 @@ ui <- dashboardPage(
 
 server <- function(input, output) {
 
-  #Set the initial characteristics of the GP
-  popMean <- 50
-  popRangeMean <- 10
-  popStdev <- 5
-
-  #Set the upper and lower limits
-  lsl <- 30
-  usl <- 70
+  observeEvent(input$helpButton, {
+    runjs("tour.init();")
+    runjs("tour.start();")
+  })
 
   # By default, the file size limit is 5MB. It can be changed by
   # setting this option. Here we'll raise limit to 9MB.
   options(shiny.maxRequestSize = 9*1024^2)
 
-  output$controlChart <- renderPlot({
-    peak <- dnorm(popMean, popMean, popStdev)
-    curve(dnorm(x, popMean, popStdev), lsl - (popMean-lsl)/2, usl + (usl-popMean)/2, col="darkblue", xlab = "", ylab = "", ylim = c (0, peak*1.1))
-    abline(v=lsl, lty=2)
-    text(lsl, peak, labels="LSL", pos=4)
-    abline(v=usl, lty=2)
-    text(usl, peak, labels="USL", pos=2)
-    arrows(popMean, 0, popMean, peak, 0, lty=4)
-    text(popMean, peak, labels="X\U33F", pos=3)
+  #Load in our big dataset (For now, use that other formula to generate a fake dataset )
+  voltages <- as.data.frame(replicate(4, rnorm(10,mean=1.31,sd=0.05)))
+
+  #Calculate X'' and R
+  sampleMeans <-rowMeans(voltages)
+  gpAve <- mean(sampleMeans)
+
+  maxMinList <- rowRanges(as.matrix(voltages))
+  sampleRanges <- maxMinList[,2]-maxMinList[,1]
+  rangeAve<- mean(sampleRanges)
+
+  #Calculate UCL and LCL
+  a2 <- 0.729
+  ucl <- gpAve + a2*rangeAve
+  lcl <- gpAve - a2*rangeAve
+
+  #Determine the intervals for each zone. Store in a dataframe for easy reference later on
+  zoneDistance <- (ucl - gpAve)/3
+
+  cZones <- c(gpAve + zoneDistance, gpAve - zoneDistance)
+  bZones <- c(gpAve + 2*zoneDistance, gpAve - 2*zoneDistance)
+  aZones <- c(ucl, lcl)
+
+  zoneDf <- data.frame(cZones,bZones,aZones)
+  colnames(zoneDf) <- c('C', 'B', 'A')
+
+  #Render the SPC Chart
+  output$SPCChart <- renderPlot({
+
+    #Plot X' with a clear centreline, dotted lines for each zone and a dashed line for UCL and LCL
+    plot(sampleMeans, type = "b", ylab = "Average of each sample", xlab = "Sample", ylim = c(lcl-0.01,ucl+0.01))
+
+    #Plot the centreline
+    abline(h=gpAve, lty=4, col="green")
+    text(gpAve, labels="Process Average", pos=4)
+
+    #Plot LCL and UCL
+    abline(h=lcl, lty=2, col="red")
+    text(lcl, labels="LCL", pos=4)
+    abline(h=ucl, lty=2, col="red")
+    text(ucl, labels="UCL", pos=4)
+
+    #Plot the zones
+    abline(h=zoneDf$C[1], lty=3, col="blue")
+    abline(h=zoneDf$C[2], lty=3, col="blue")
+
+    abline(h=zoneDf$B[1], lty=3, col="blue")
+    abline(h=zoneDf$B[2], lty=3, col="blue")
+
+    text(gpAve + zoneDistance/2, labels="Zone C", pos=4)
+    text(gpAve - zoneDistance/2, labels="Zone C", pos=4)
+
+    text(gpAve + 1.5*zoneDistance, labels="Zone B", pos=4)
+    text(gpAve - 1.5*zoneDistance, labels="Zone B", pos=4)
+
+    text(gpAve + 2.5*zoneDistance, labels="Zone A", pos=4)
+    text(gpAve - 2.5*zoneDistance, labels="Zone A", pos=4)
   })
 
-  output$tableView = renderDataTable(
-    data.frame(Group=1:100, Mean=rnorm(100, popMean, popStdev), Range=rnorm(100, popRangeMean, popStdev)),
-    options = list(pageLength = 10)
+  loadedGroup <- reactiveValues(data=NULL)
+
+  #Create eventhandlers for actionButtons
+  observeEvent(input$addSampleActionButton, {
+    addSampleContents <- as.numeric(as.character(input$addSampleTextInput))
+    if(!is.null(addSampleContents) && !is.na(addSampleContents)) {
+      loadedGroup$data <- rbind(data.frame(Point=as.numeric(as.character(loadedGroup$data[,1]))),data.frame(Point=addSampleContents))
+    }
+  })
+
+  observeEvent(input$removeSamplesActionButton, {
+    remaining <- data.frame(Point=loadedGroup$data[!rownames(loadedGroup$data) %in% input$addGroupTable_rows_selected,])
+    loadedGroup$data <- remaining
+  })
+
+  observeEvent(input$clearGroupActionButton, {
+    loadedGroup$data = NULL
+  })
+
+  addedGroup <- eventReactive(input$addGroupActionButton, {
+    if (is.null(loadedGroup$data)) return(NULL)
+    data.frame(Point=as.numeric(as.character(loadedGroup$data[,1])))
+  })
+
+  #Render the DataTables
+  output$tableView = DT::renderDataTable(
+    data.frame(Mean=round(sampleMeans, digits = 3), Range=round(sampleRanges, digits = 3)),
+    options = list(pageLength = 10, pagingType = "full", searching = FALSE)
   )
 
+  output$addGroupTable <- DT::renderDataTable(server = TRUE,escape=F,options = list(paging = FALSE, searching = FALSE),{
+    if (is.null(loadedGroup$data)) return(NULL)
+    loadedGroup$data
+  })
+
+  tableViewProxy <- DT::dataTableProxy('tableView')
+  addGroupTableProxy <- DT::dataTableProxy('addGroupTable')
 
   output$contents <- renderText({
     # input$file1 will be NULL initially. After the user selects
@@ -167,42 +254,36 @@ server <- function(input, output) {
     # column will contain the local filenames where the data can
     # be found.
 
+    inGroup <- addedGroup()[,1]
+
     inFile <- input$file1
 
-    if (is.null(inFile))
+    if (is.null(inFile) && is.null(inGroup)) {
       return(NULL)
-
-    #Read in the new sample
-    inputData <- read.csv(inFile$datapath)
-    newSample <- inputData[,2]
+    } else if (!(is.null(inFile))) {
+      #Read in the new sample
+      inputData <- read.csv(inFile$datapath)
+      newSample <- inputData[,2]
+    } else if (!(is.null(inGroup))) {
+      newSample <- inGroup
+      loadedGroup$data = NULL
+    }
 
     #Calculate the stats associated with the new sample
     sampleMean <- mean(newSample)
     sampleSize <- length(newSample)
     sampleRange <- diff(range(newSample))
-    stDevOptions <- c(NaN,1.128,1.693,2.059,2.326,2.534,2.704,2.847,2.970,3.078)
+    stDevOptions <- c(1,1.128,1.693,2.059,2.326,2.534,2.704,2.847,2.970,3.078)
     sampleStDev <- stDevOptions[sampleSize]
 
     #Update the population stats
-    popMean <- (popMean + sampleMean)/2
-    popRangeMean <- (popRangeMean + sampleRange)/2
+    gpAve <- (gpAve + sampleMean)/2
+    rangeAve <- (rangeAve + sampleRange)/2
 
     #Calculate the process capability indices
-    cpl <- (popMean - lsl)/(3 * sampleStDev)
-    cul <- (usl - popMean)/(3 * sampleStDev)
+    cpl <- (gpAve - lcl)/(3 * sampleStDev)
+    cul <- (ucl - gpAve)/(3 * sampleStDev)
     cpk <- min(cpl,cul)
-
-    print("popMean")
-    print(popMean)
-    print("lsl")
-    print(lsl)
-    print("usl")
-    print(usl)
-
-    print("sampleMean")
-    print(sampleMean)
-    print("sampleStDev")
-    print(sampleStDev)
 
     #Render valueBoxes
     output$cpkBox <- renderValueBox({
@@ -233,30 +314,32 @@ server <- function(input, output) {
 
     #Tell the user if defective material is being made or not
     if(cpk<1){
-      displayMessage <- paste("Your process capability is",cpk,"so defective materials are being made",sep = " ")
+      displayMessage <- paste("Your process capability is",round(cpk, digits = 3),"so defective materials are being made",sep = " ")
       output$bad <- renderText({displayMessage})
       output$good <- renderText({""})
     }
     else{
-      displayMessage <- paste("Your process capability value is",cpk,"so defective materials are not being made",sep = " ")
+      displayMessage <- paste("Your process capability value is",round(cpk, digits = 3),"so defective materials are not being made",sep = " ")
       output$good <- renderText({displayMessage})
       output$bad <- renderText({""})
     }
 
     #Calculate the defective material percentage
-    upperArea <- pnorm(usl, mean = sampleMean, sampleStDev, lower.tail = FALSE, log.p = FALSE)
-    lowerArea <- pnorm(lsl, mean = sampleMean, sampleStDev, lower.tail = TRUE, log.p = FALSE)
+    upperArea <- pnorm(ucl, mean = sampleMean, sampleStDev, lower.tail = FALSE, log.p = FALSE)
+    lowerArea <- pnorm(lcl, mean = sampleMean, sampleStDev, lower.tail = TRUE, log.p = FALSE)
     totalAreaPercentage <- (upperArea + lowerArea) * 100
 
-    defPercentageMessage <- paste("The probability of obtaining a defective item:", totalAreaPercentage,"%",sep = " ")
+    defPercentageMessage <- paste("The probability of obtaining a defective item:", round(totalAreaPercentage, digits = 3),"%",sep = " ")
     output$defPercentage <- renderText({defPercentageMessage})
 
     contentsMessage <- ""
 
 
     #use p norm
-    #for usl, lower tail false
-    #for lsl, lower tail true
+    #for ucl, lower tail false
+    #for lcl, lower tail true
+
+    loadedGroup = NULL
 
   })
 }
